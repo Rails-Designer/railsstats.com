a9588978445ef5730ed9b0dfebc48a2f87aa3f01
eelco <git@eelcoj.com>
2025-01-11 19:17:31 +0700

    Putting on the brakes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,22 @@ class PagesController < ApplicationController
   rescue_from ActionView::MissingTemplate, with: :not_found
 
   def show
-    path = "#{params[:page]}"
-    md_path = Rails.root.join("app/views/pages/#{path}.md")
+    markdown_path = Rails.root.join("app/views/pages/#{sanitized_path}.md")
 
-    if File.exist?(md_path)
-      render html: markdown(File.read(md_path)).html_safe, layout: "page"
+    if File.exist?(markdown_path)
+      render html: markdown(File.read(markdown_path)).html_safe, layout: "page"
 
       return
     end
 
-    render template: "pages/#{path}"
+    render template: "pages/#{sanitized_path}"
   rescue ActionView::MissingTemplate
     not_found
   end
 
   private
+
+  def sanitized_path = params[:page].to_s.gsub(/[^0-9A-Za-z\-_]/, "")
 
   def not_found = render file: Rails.public_path.join("404.html"), status: :not_found, layout: false
 
