9e18fe132170b41c15baae1a5afa3d8eddf2f680
eelco <git@eelcoj.com>
2025-01-12 10:13:50 +0700

    Little reshuffle

--- app/models/concerns/decoration.rb
+++ app/models/concerns/decoration.rb
@@ -1,6 +1,6 @@
 module Decoration
   def decorate(with: nil)
-    decorator_class = with || "#{self.class}Decorator".constantize
+    decorator_class = with || "#{self.class}::Decorator".constantize
 
     decorator_class.new(self)
   end
--- app/models/project/decorator.rb
+++ app/models/project/decorator.rb
@@ -1,4 +1,4 @@
-class ProjectDecorator < SimpleDelegator
+class Project::Decorator < SimpleDelegator
   def display_website = URI.parse(website).host
 
   def display_founded_at = founded_at.strftime("%B %Y")
